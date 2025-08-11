ce0f09a6480147758675b827c785367bd1909e98
Eric dev <eketchadji@gmail.com>
2025-08-11 13:14:57 +0100

    first

---
+++ script.js
@@ -0,0 +1,56 @@
+// Animation fade-in lors du scroll
+document.addEventListener('DOMContentLoaded', function () {
+        const sections = document.querySelectorAll('.fade-in-section');
+        const observer = new IntersectionObserver((entries) => {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    entry.target.classList.add('visible');
+                } else {
+                    entry.target.classList.remove('visible');
+                }
+            });
+        }, {
+            threshold: 0.15
+        });
+        sections.forEach(section => {
+            observer.observe(section);
+        });
+});
+const phrases=["Bonjour je suis Eric Ketchadji.",
+    "Étudiant à l'école Nationale Superieur Polytechnique de Maroua.",
+    "développeur passionné, créatif et curieux,",
+    "toujours prêt à relever de nouveaux défis numériques."
+];
+let phraseIndex=0;
+let charIndex=0;
+let isDeleting=false;
+const speed=100;
+const eraseSpeed=50;
+const delai=1500;
+
+
+function Writer(){
+    const currentPhrase=phrases[phraseIndex];
+    const target=document.getElementById("writer");
+
+    if(!isDeleting){
+        target.textContent=currentPhrase.substring(0,charIndex + 1);
+        charIndex++;
+        if(charIndex===currentPhrase.length){
+            isDeleting=true;
+            setTimeout(Writer,delai);
+            return;
+        }
+    }else{
+        target.textContent=currentPhrase.substring(0,charIndex - 1);
+        charIndex--;
+
+        if(charIndex===0){
+            isDeleting=false;
+            phraseIndex=(phraseIndex+1)%phrases.length;
+        }
+    }
+    setTimeout(Writer,isDeleting ? eraseSpeed:speed);
+}
+document.addEventListener("DOMContentLoaded",Writer);
+
